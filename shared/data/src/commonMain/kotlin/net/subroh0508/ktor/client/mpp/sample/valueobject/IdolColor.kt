e401416d5cf8ce6e08eef61447f3b69fd60c69dc
subroh0508 <subroh.0508@gmail.com>
2020-02-24 02:12:53 +0900

    [edit]Add isBrighter property to IdolColor

--- shared/data/src/commonMain/kotlin/net/subroh0508/ktor/client/mpp/sample/valueobject/IdolColor.kt
+++ shared/data/src/commonMain/kotlin/net/subroh0508/ktor/client/mpp/sample/valueobject/IdolColor.kt
@@ -4,4 +4,12 @@ data class IdolColor(
     val id: String,
     val name: String,
     val hexColor: String
-)
+) {
+    val isBrighter: Boolean get() {
+        val red = hexColor.substring(0, 2).toInt(16)
+        val green = hexColor.substring(2, 4).toInt(16)
+        val blue = hexColor.substring(4, 6).toInt(16)
+
+        return (red * 0.299 + green * 0.587 + blue * 0.114) < 186
+    }
+}
