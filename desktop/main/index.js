7c7f7184c6ec47bc20ad7227c11941f39c447738
subroh0508 <subroh.0508@gmail.com>
2020-02-24 19:11:12 +0900

    [add]Add desktop directory(used by Electron)

---
+++ desktop/main/index.js
@@ -0,0 +1,78 @@
+import path from 'path';
+import { app, BrowserWindow } from 'electron';
+import electronDebug from 'electron-debug';
+import devtoolsInstaller, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer';
+
+const dist = path.resolve(__dirname, '../../../web/build/processedResources/Js/main');
+
+// ウインドウオブジェクトのグローバル参照を保持してください。さもないと、そのウインドウは
+// JavaScript オブジェクトがガベージコレクションを行った時に自動的に閉じられます。
+let win;
+
+if (process.env.NODE_ENV === 'development') {
+    electronDebug();
+}
+
+const installExtensions = async () => {
+    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+    const extensions = [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS];
+
+    return Promise.all(
+        extensions.map(name => devtoolsInstaller(name, forceDownload))
+    ).catch(console.log);
+};
+
+const createWindow = async () => {
+    if (process.env.NODE_ENV === 'development') {
+        await installExtensions();
+    }
+
+    // browser window を生成する
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+    });
+
+    // そしてこのアプリの index.html をロード
+    const port = process.env.PORT || 8088;
+    const winUrl = process.env.NODE_ENV = 'development' ? `http://localhost:${port}` : `file://${dist}/index.html`;
+
+    win.loadURL(winUrl);
+
+    // 開発者ツールを開く
+    win.webContents.openDevTools();
+
+    // ウィンドウが閉じられた時に発火
+    win.on('closed', () => {
+        // ウインドウオブジェクトの参照を外す。
+        // 通常、マルチウインドウをサポートするときは、
+        // 配列にウインドウを格納する。
+        // ここは該当する要素を削除するタイミング。
+        win = null;
+    });
+};
+
+// このイベントは、Electronが初期化処理と
+// browser windowの作成を完了した時に呼び出されます。
+// 一部のAPIはこのイベントが発生した後にのみ利用できます。
+app.on('ready', createWindow);
+
+// 全てのウィンドウが閉じられた時に終了する
+app.on('window-all-closed', () => {
+    // macOSでは、ユーザが Cmd + Q で明示的に終了するまで、
+    // アプリケーションとそのメニューバーは有効なままにするのが一般的です。
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // macOSでは、ユーザがドックアイコンをクリックしたとき、
+    // そのアプリのウインドウが無かったら再作成するのが一般的です。
+    if (win === null) {
+        createWindow();
+    }
+});
