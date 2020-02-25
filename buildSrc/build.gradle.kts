0b6df3280663ba49cfca769890ec4271215e5ec9
subroh0508 <subroh.0508@gmail.com>
2020-02-26 02:09:02 +0900

    [update]Update Kotlin -> 1.3.70-eap-274 on buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,9 +5,10 @@ plugins {
 repositories {
     jcenter()
     google()
+    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 dependencies {
     implementation("com.android.tools.build:gradle:3.5.2")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70-eap-274")
 }
