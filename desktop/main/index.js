3a995072e8e77d81f402976eee603cbdfa9d5521
subroh0508 <subroh.0508@gmail.com>
2020-02-25 21:18:57 +0900

    [edit]Fix indent 4 spaces to 2 spaces

--- desktop/main/index.js
+++ desktop/main/index.js
@@ -10,49 +10,49 @@ const dist = path.resolve(__dirname, '../../../web/build/processedResources/Js/m
 let win;
 
 if (process.env.NODE_ENV === 'development') {
-    electronDebug();
+  electronDebug();
 }
 
 const installExtensions = async () => {
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-    const extensions = [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS];
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS];
 
-    return Promise.all(
-        extensions.map(name => devtoolsInstaller(name, forceDownload))
-    ).catch(console.log);
+  return Promise.all(
+    extensions.map(name => devtoolsInstaller(name, forceDownload))
+  ).catch(console.log);
 };
 
 const createWindow = async () => {
-    if (process.env.NODE_ENV === 'development') {
-        await installExtensions();
-    }
+  if (process.env.NODE_ENV === 'development') {
+    await installExtensions();
+  }
 
-    // browser window を生成する
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        },
-    });
+  // browser window を生成する
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+  });
 
-    // そしてこのアプリの index.html をロード
-    const port = process.env.PORT || 8088;
-    const winUrl = process.env.NODE_ENV = 'development' ? `http://localhost:${port}` : `file://${dist}/index.html`;
+  // そしてこのアプリの index.html をロード
+  const port = process.env.PORT || 8088;
+  const winUrl = process.env.NODE_ENV = 'development' ? `http://localhost:${port}` : `file://${dist}/index.html`;
 
-    win.loadURL(winUrl);
+  win.loadURL(winUrl);
 
-    // 開発者ツールを開く
-    win.webContents.openDevTools();
+  // 開発者ツールを開く
+  win.webContents.openDevTools();
 
-    // ウィンドウが閉じられた時に発火
-    win.on('closed', () => {
-        // ウインドウオブジェクトの参照を外す。
-        // 通常、マルチウインドウをサポートするときは、
-        // 配列にウインドウを格納する。
-        // ここは該当する要素を削除するタイミング。
-        win = null;
-    });
+  // ウィンドウが閉じられた時に発火
+  win.on('closed', () => {
+    // ウインドウオブジェクトの参照を外す。
+    // 通常、マルチウインドウをサポートするときは、
+    // 配列にウインドウを格納する。
+    // ここは該当する要素を削除するタイミング。
+    win = null;
+  });
 };
 
 // このイベントは、Electronが初期化処理と
@@ -62,17 +62,17 @@ app.on('ready', createWindow);
 
 // 全てのウィンドウが閉じられた時に終了する
 app.on('window-all-closed', () => {
-    // macOSでは、ユーザが Cmd + Q で明示的に終了するまで、
-    // アプリケーションとそのメニューバーは有効なままにするのが一般的です。
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  // macOSでは、ユーザが Cmd + Q で明示的に終了するまで、
+  // アプリケーションとそのメニューバーは有効なままにするのが一般的です。
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    // macOSでは、ユーザがドックアイコンをクリックしたとき、
-    // そのアプリのウインドウが無かったら再作成するのが一般的です。
-    if (win === null) {
-        createWindow();
-    }
+  // macOSでは、ユーザがドックアイコンをクリックしたとき、
+  // そのアプリのウインドウが無かったら再作成するのが一般的です。
+  if (win === null) {
+    createWindow();
+  }
 });
