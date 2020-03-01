817de741970a32d43a618724d788308bb6a1d938
subroh0508 <subroh.0508@gmail.com>
2020-03-01 18:52:07 +0900

    [edit]Commit fragments

--- android/features/idol/src/main/kotlin/net/subroh0508/colormaster/idol/ui/IdolsFragment.kt
+++ android/features/idol/src/main/kotlin/net/subroh0508/colormaster/idol/ui/IdolsFragment.kt
@@ -1,7 +1,23 @@
 package net.subroh0508.colormaster.idol.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import net.subroh0508.colormaster.idol.R
 
 class IdolsFragment : Fragment(R.layout.fragment_idols) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupIdolsFragment()
+    }
+
+    private fun setupIdolsFragment() {
+        val fragment = BottomSheetIdolsFragment()
+
+        childFragmentManager.beginTransaction()
+            .replace(R.id.fragment_bottom_sheet_idols, fragment)
+            .disallowAddToBackStack()
+            .commit()
+    }
 }
--- android/features/idol/src/main/kotlin/net/subroh0508/colormaster/idol/ui/MainIdolsFragment.kt
+++ android/features/idol/src/main/kotlin/net/subroh0508/colormaster/idol/ui/MainIdolsFragment.kt
@@ -1,8 +1,23 @@
 package net.subroh0508.colormaster.idol.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import net.subroh0508.colormaster.idol.R
 
 class MainIdolsFragment : Fragment(R.layout.fragment_main_idols) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        setupIdolsFragment()
+    }
+
+    private fun setupIdolsFragment() {
+        val fragment = IdolsFragment()
+
+        childFragmentManager.beginTransaction()
+            .replace(R.id.fragment_idols, fragment)
+            .commit()
+    }
 }
