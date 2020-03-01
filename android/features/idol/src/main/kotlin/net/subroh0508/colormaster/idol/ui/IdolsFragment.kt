73565ad61695918f4c21f58ddf657d93d56e9ee2
subroh0508 <subroh.0508@gmail.com>
2020-03-01 22:17:19 +0900

    [edit]Add setting BottomSheet's ShapeAppearance method

--- android/features/idol/src/main/kotlin/net/subroh0508/colormaster/idol/ui/IdolsFragment.kt
+++ android/features/idol/src/main/kotlin/net/subroh0508/colormaster/idol/ui/IdolsFragment.kt
@@ -3,11 +3,18 @@ package net.subroh0508.colormaster.idol.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 import net.subroh0508.colormaster.idol.R
+import net.subroh0508.colormaster.idol.databinding.FragmentIdolsBinding
 
 class IdolsFragment : Fragment(R.layout.fragment_idols) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentIdolsBinding.bind(view)
+
+        initBottomSheetShapeAppearance(binding)
 
         setupIdolsFragment()
     }
@@ -20,4 +27,34 @@ class IdolsFragment : Fragment(R.layout.fragment_idols) {
             .disallowAddToBackStack()
             .commit()
     }
+
+    // BottomSheetを開いた時に角丸が効かなくなるためコードで対応
+    // XMLでの対応は、Backdropがコンポーネントとして提供されるまで待つ必要がありそう
+    // 参考1: https://github.com/DroidKaigi/conference-app-2020/issues/104
+    // 参考2: https://github.com/material-components/material-components-android/pull/437
+    private fun initBottomSheetShapeAppearance(binding: FragmentIdolsBinding) {
+        val shapeAppearanceModel =
+            ShapeAppearanceModel.Builder()
+                .setTopLeftCorner(
+                    CornerFamily.ROUNDED,
+                    resources.getDimension(R.dimen.bottom_sheet_corner_radius)
+                )
+                .setTopRightCorner(
+                    CornerFamily.ROUNDED,
+                    resources.getDimension(R.dimen.bottom_sheet_corner_radius)
+                )
+                .build()
+        /**
+         * FrontLayer elevation is 1dp
+         * https://material.io/components/backdrop/#anatomy
+         */
+        val materialShapeDrawable = MaterialShapeDrawable.createWithElevationOverlay(
+            requireActivity(),
+            resources.getDimension(R.dimen.bottom_sheet_elevation)
+        ).apply {
+            setShapeAppearanceModel(shapeAppearanceModel)
+        }
+        binding.fragmentBottomSheetIdols.background = materialShapeDrawable
+    }
+
 }
