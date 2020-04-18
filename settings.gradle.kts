9e1feaabdfc14459c7f6a572d3266e2435bf9898
subroh0508 <subroh.0508@gmail.com>
2020-04-18 13:25:13 +0900

    [add]Add :shared:utilities module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ include(
     ":android:features:idol",
     ":android:widget",
     ":web:app",
+    ":shared:utilities",
     ":shared:model",
     ":shared:components:core",
     ":shared:infra:repository",
---
+++ shared/utilities/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform")
+    `android-library`
+    id("kotlinx-serialization")
+}
+
+kotlin {
+    android("android")
+    js { nodejs {} }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(Libraries.Kotlin.common)
+                implementation(Libraries.Coroutines.common)
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation(Libraries.Kotlin.android)
+                implementation(Libraries.Coroutines.android)
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation(Libraries.Kotlin.js)
+                implementation(Libraries.Coroutines.js)
+            }
+        }
+    }
+}
