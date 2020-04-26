9fc779c8fe7d75c7a8f0e000b0f19457d813446c
subroh0508 <subroh.0508@gmail.com>
2020-04-26 18:45:50 +0900

    [add]Add ThemeProvider component

---
+++ web/app/src/main/kotlin/themes/ThemeProvider.kt
@@ -0,0 +1,57 @@
+package themes
+
+import kotlinx.css.Color
+import materialui.styles.createMuiTheme
+import materialui.styles.muitheme.options.palette
+import materialui.styles.palette.PaletteType
+import materialui.styles.palette.options.*
+import materialui.styles.themeprovider.themeProvider
+import react.*
+
+@Suppress("FunctionName")
+fun RBuilder.ThemeProvider(handler: RHandler<ThemeProviderProps>) = child(ThemeProviderComponent, handler = handler)
+
+private val ThemeProviderComponent = functionalComponent<ThemeProviderProps> { props ->
+    val theme = useMemo({ createTheme(props.paletteType) }, arrayOf(props.paletteType))
+
+    themeProvider(theme) {
+        props.children()
+    }
+}
+
+external interface ThemeProviderProps : RProps {
+    var paletteType: PaletteType
+}
+
+private fun createTheme(paletteType: PaletteType) = createMuiTheme {
+    palette {
+        type = paletteType
+
+        when (paletteType) {
+            PaletteType.light -> {
+                primary {
+                    main = Color("#e65100")
+                }
+                secondary {
+                    main = Color("#fff09f")
+                }
+                background {
+                    paper = Color.white
+                    default = Color("#fafafa")
+                }
+            }
+            PaletteType.dark -> {
+                primary {
+                    main = Color("#ffcc80")
+                }
+                secondary {
+                    main = Color("#fff09f")
+                }
+                background {
+                    paper = Color("#303030")
+                    default = Color("#121212")
+                }
+            }
+        }
+    }
+}
