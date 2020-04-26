8874df34f54eb7626a5cb39a44514b805125eb2a
subroh0508 <subroh.0508@gmail.com>
2020-04-27 02:26:12 +0900

    [edit]Enable dark mode depends on browser

--- web/app/src/main/kotlin/components/templates/AppFrame.kt
+++ web/app/src/main/kotlin/components/templates/AppFrame.kt
@@ -3,19 +3,10 @@ package components.templates
 import components.atoms.MenuComponent
 import components.atoms.appBarTop
 import isExpandAppBar
-import kotlinext.js.jsObject
 import kotlinx.css.*
-import kotlinx.css.properties.BoxShadows
-import materialui.components.appbar.appBar
-import materialui.components.appbar.enums.AppBarColor
-import materialui.components.appbar.enums.AppBarPosition
-import materialui.styles.breakpoint.Breakpoint
-import materialui.styles.breakpoint.up
-import materialui.styles.makeStyles
 import materialui.styles.palette.PaletteType
-import materialui.styles.palette.paper
+import materialui.useMediaQuery
 import react.*
-import react.dom.div
 import react.router.dom.useHistory
 import themes.ThemeProvider
 
@@ -24,12 +15,17 @@ val APP_BAR_SM_UP = 408.px
 fun RBuilder.appFrame(handler: RHandler<RProps>) = child(AppFrameComponent, handler = handler)
 
 private val AppFrameComponent = functionalComponent<RProps> { props ->
+    val prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
     val (appState, setAppState) = useState(AppState(
         themeType = PaletteType.light,
         lang = "ja",
         openDrawer = false
     ))
     val history = useHistory()
+
+    useEffect(listOf(prefersDarkMode)) {
+        setAppState(appState.copy(themeType = if (prefersDarkMode) PaletteType.dark else PaletteType.light))
+    }
 
     ThemeProvider {
         attrs.paletteType = appState.themeType
