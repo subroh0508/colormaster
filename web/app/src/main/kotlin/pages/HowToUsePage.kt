9ba98949e32d7d6023cbab6f60e0716421b8baa3
subroh0508 <subroh.0508@gmail.com>
2020-04-27 00:10:06 +0900

    [edit]Add description to HowToUsePage

--- web/app/src/main/kotlin/pages/HowToUsePage.kt
+++ web/app/src/main/kotlin/pages/HowToUsePage.kt
@@ -1,27 +1,125 @@
 package pages
 
-import materialui.components.paper.paper
-import materialui.components.typography.typographyH4
+import kotlinx.css.Display
+import kotlinx.css.VerticalAlign
+import kotlinx.css.display
+import kotlinx.css.verticalAlign
+import materialui.components.card.card
+import materialui.components.cardcontent.cardContent
+import materialui.components.cardheader.cardHeader
+import materialui.components.icon.icon
+import materialui.components.typography.enums.TypographyVariant
+import materialui.components.typography.typography
 import materialui.components.typography.typographyH5
 import react.RBuilder
-import react.RProps
-import react.child
-import react.dom.p
-import react.functionalComponent
+import react.dom.*
+import styled.css
+import styled.styledSpan
 
 @Suppress("FunctionName")
 fun RBuilder.HowToUsePage() = StaticPage {
-    paper {
-        typographyH4 {
-            +"何ができるの？"
-        }
+    card {
+        attrs.className = CARD_ELEMENT_CLASS_NAME
+        attrs["variant"] = "outlined"
 
-        p {
-            +"THE IDOLM@STERシリーズに登場するアイドルのイメージカラーを検索、PC・スマホ端末上でプレビューを表示することができます"
+        cardHeader {
+            attrs.title {
+                typographyH5 { +"何ができるの？" }
+            }
+        }
+        cardContent {
+            p {
+                +"THE IDOLM@STERシリーズに登場するアイドルを検索し、PC・スマホ端末上でイメージカラーのプレビューを表示することができます。"
+            }
         }
     }
 
-    paper {
+    card {
+        attrs.className = CARD_ELEMENT_CLASS_NAME
+        attrs["variant"] = "outlined"
 
+        cardHeader {
+            attrs.title {
+                typographyH5 { +"どんな機能があるの？" }
+            }
+        }
+
+        cardContent {
+            typography {
+                attrs.variant = TypographyVariant.subtitle1
+                styledSpan {
+                    css { display = Display.flex; verticalAlign = VerticalAlign.middle }
+
+                    icon { +"palette_icon" }
+                    strong { +"プレビューモード" }
+                }
+            }
+
+            p {
+                +"「イメージカラー」「アイドル名」「カラーコード」を画面いっぱいに表示します。"
+            }
+            p {
+                +"買い物中に目についた、"
+                strong { +"担当カラーっぽいTシャツ・食器・文房具etc.の色を瞬時にスマホで確認することができます！" }
+                +"最高ですね👍"
+            }
+
+            typography {
+                attrs.variant = TypographyVariant.subtitle1
+                styledSpan {
+                    css { display = Display.flex; verticalAlign = VerticalAlign.middle }
+
+                    icon { +"highlight_icon" }
+                    strong { +"ペンライトモード" }
+                }
+            }
+
+            p {
+                +"「イメージカラー」のみを画面いっぱいに表示します。"
+            }
+            p {
+                +"アニクラやDJバーで突如流れ出す担当曲！だけど今日はペンライトを持ってきていない😭"
+            }
+            p {
+                +"こんな悲劇とはもうおさらば、"
+                strong { +"あなたのスマホが瞬時にペンライトに生まれ変わります！" }
+                +"優勝間違いなし🏆"
+            }
+        }
+    }
+
+    card {
+        attrs.className = CARD_ELEMENT_CLASS_NAME
+        attrs["variant"] = "outlined"
+
+        cardHeader {
+            attrs.title {
+                typographyH5 { +"使い方" }
+            }
+        }
+        cardContent {
+            ol {
+                li {
+                    +"検索画面でアイドルを検索する"
+                }
+                li {
+                    +"プレビューしたいアイドルを選択する"
+                }
+                li {
+                    strong { +"「プレビュー」" }
+                    +"、または"
+                    strong { +"「ペンライト」" }
+                    +"ボタンをタップする"
+                }
+            }
+
+            p {
+                +"また、"
+                strong { +"「プレビューしたいアイドルをダブルタップ」" }
+                +"することで、"
+                strong { +"ペンライトモードをショートカット起動" }
+                +"させることができます。"
+            }
+        }
     }
 }
--- web/app/src/main/kotlin/pages/StaticPage.kt
+++ web/app/src/main/kotlin/pages/StaticPage.kt
@@ -1,8 +1,11 @@
 package pages
 
+import kotlinx.css.*
 import materialui.components.container.container
 import materialui.styles.makeStyles
 import materialui.styles.mixins.toolbar
+import materialui.styles.muitheme.spacing
+import materialui.styles.palette.default
 import react.*
 import react.dom.div
 
@@ -12,15 +15,34 @@ fun RBuilder.StaticPage(handler: RHandler<RProps>) = child(StaticPageComponent, 
 private val StaticPageComponent = functionalComponent<RProps> { props ->
     val classes = useStyles()
 
-    div(classes.toolbar) {}
-    container { props.children() }
+    div(classes.root) {
+        div(classes.toolbar) {}
+        container {
+            attrs.classes(classes.container)
+            props.children()
+        }
+    }
 }
+
+const val CARD_ELEMENT_CLASS_NAME = "card-element"
 
 private external interface StaticPageStyle {
     val root: String
     val toolbar: String
+    val container: String
 }
 
 private val useStyles = makeStyles<StaticPageStyle> {
+    "root" {
+        height = 100.vh
+        backgroundColor = theme.palette.background.default
+    }
     "toolbar"(theme.mixins.toolbar)
+    "container" {
+        paddingTop = theme.spacing(2)
+
+        descendants(".$CARD_ELEMENT_CLASS_NAME") {
+            marginBottom = theme.spacing(4)
+        }
+    }
 }
