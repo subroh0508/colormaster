3ff6dcfa3f5c12112fd1c247507907e4d796a88c
subroh0508 <subroh.0508@gmail.com>
2020-05-27 23:24:39 +0900

    [fix]Fix devServer script

--- web/app/webpack.config.d/02.devServer.js
+++ web/app/webpack.config.d/02.devServer.js
@@ -1,7 +1,5 @@
 if (config.mode === 'development') {
-  config.devServer = {
-    historyApiFallback: true,
-    host: '0.0.0.0',
-    disableHostCheck: true,
-  };
+    config.devServer.historyApiFallback = true;
+    config.devServer.host = '0.0.0.0';
+    config.devServer.disableHostCheck = true;
 }
