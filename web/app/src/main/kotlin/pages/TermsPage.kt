6ddbd661d78c552e2b60da74e4ef4ed642745085
subroh0508 <subroh.0508@gmail.com>
2020-06-09 21:50:27 +0900

    [add]Add translation to TermsPage.kt

--- web/app/src/main/kotlin/pages/TermsPage.kt
+++ web/app/src/main/kotlin/pages/TermsPage.kt
@@ -6,30 +6,35 @@ import materialui.components.cardcontent.cardContent
 import materialui.components.cardheader.cardHeader
 import materialui.components.typography.typographyH5
 import react.RBuilder
+import react.RProps
+import react.child
 import react.dom.p
+import react.functionalComponent
+import utilities.Trans
+import utilities.invoke
+import utilities.useTranslation
 
 @Suppress("FunctionName")
-fun RBuilder.TermsPage() = StaticPage {
+fun RBuilder.TermsPage() = StaticPage { child(TermComponent) }
+
+private val TermComponent = functionalComponent<RProps> {
+    val (t, _) = useTranslation()
+
     card {
         attrs.className = CARD_ELEMENT_CLASS_NAME
         attrs["variant"] = "outlined"
 
         cardHeader {
             attrs.title {
-                typographyH5 { +"利用規約" }
+                typographyH5 { +t("about.terms.top.title") }
             }
         }
         cardContent {
-            p {
-                +"この利用規約は、"
-                link(
-                    href = "https://twitter.com/subroh_0508",
-                    label = "にしこりさぶろ〜"
-                )
-                +"(以下「開発者」という)が、COLOR M@STER(以下「当アプリ」という)で提供するサービスの利用条件を定めるものです。"
-            }
-            p {
-                +"利用者の皆様(以下「利用者」という)が当アプリを利用する場合、この規約(以下「本規約」という)に定める事項を承諾したものとみなします。"
+            Trans {
+                attrs.i18nKey = "about.terms.top.description"
+
+                p { link(href = "https://twitter.com/subroh_0508") }
+                p { +"dummy" }
             }
         }
     }
@@ -40,15 +45,15 @@ fun RBuilder.TermsPage() = StaticPage {
 
         cardHeader {
             attrs.title {
-                typographyH5 { +"1. 免責事項" }
+                typographyH5 { +t("about.terms.disclaimer.title") }
             }
         }
         cardContent {
-            p {
-                +"開発者は当アプリにおける情報の正確性、特定の目的への適合性，セキュリティなどに関する欠陥，エラーやバグ等がないことを保証しておりません。"
-            }
-            p {
-                +"また、当アプリを利用したことで生じたあらゆる損害・不利益等に対して、開発者はいかなる責任も負いません。"
+            Trans {
+                attrs.i18nKey = "about.terms.disclaimer.description"
+
+                p { +"dummy" }
+                p { +"dummy" }
             }
         }
     }
@@ -59,18 +64,16 @@ fun RBuilder.TermsPage() = StaticPage {
 
         cardHeader {
             attrs.title {
-                typographyH5 { +"2. Cookieの利用について" }
+                typographyH5 { +t("about.terms.cookie.title") }
             }
         }
         cardContent {
-            p {
-                +"当アプリでは、当アプリの改善を図るため、Google社のサービスであるGoogle Analyticsを利用し、利用者の閲覧情報を収集しています。"
-            }
-            p {
-                +"利用者がブラウザを利用してウェブサイトにアクセスすると、これらサービスがCookieを利用してデータの収集、処理をおこないます。このサービスが収集、処理するデータに、利用者個人を特定する情報は含まれておりません。"
-            }
-            p {
-                +"また、利用者は、当アプリを利用することで、Google社が行うこうしたデータ処理に対し許可を与えたものとみなします。"
+            Trans {
+                attrs.i18nKey = "about.terms.cookie.description"
+
+                p { +"dummy" }
+                p { +"dummy" }
+                p { +"dummy" }
             }
         }
     }
