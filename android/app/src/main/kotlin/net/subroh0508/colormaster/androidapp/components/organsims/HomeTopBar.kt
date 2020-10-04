a55447cdac37d05b101d82826e5f2e1fc6d2f184
subroh0508 <subroh.0508@gmail.com>
2020-10-04 19:06:43 +0900

    [add]Add HomTopBar

---
+++ android/app/src/main/kotlin/net/subroh0508/colormaster/androidapp/components/organsims/HomeTopBar.kt
@@ -0,0 +1,41 @@
+package net.subroh0508.colormaster.androidapp.components.organsims
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.preferredHeight
+import androidx.compose.foundation.layout.preferredWidth
+import androidx.compose.material.DrawerState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import net.subroh0508.colormaster.androidapp.R
+import net.subroh0508.colormaster.androidapp.components.molecules.ScrollableTabs
+
+@Composable
+fun HomeTopBar(titles: Array<String>, drawerState: DrawerState) {
+    Row(
+        Modifier.preferredHeight(56.dp)
+            .background(MaterialTheme.colors.background),
+    ) {
+        Image(
+            asset = vectorResource(R.drawable.ic_menu),
+            colorFilter = ColorFilter.tint(MaterialTheme.colors.onSurface),
+            modifier = Modifier.align(Alignment.CenterVertically)
+                .clickable(onClick = { drawerState.open() }),
+        )
+        Spacer(Modifier.preferredWidth(Dp(16F)))
+
+        ScrollableTabs(
+            titles,
+            modifier = Modifier.weight(1F).align(Alignment.CenterVertically),
+        )
+    }
+}
