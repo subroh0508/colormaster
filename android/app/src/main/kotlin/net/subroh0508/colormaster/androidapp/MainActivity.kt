cbd065da013aafb47477f6d3e8dda82d9a34f210
subroh0508 <subroh.0508@gmail.com>
2020-10-10 00:42:37 +0900

    [edit]Show ColorLists on frontLayerContent

--- android/app/src/main/kotlin/net/subroh0508/colormaster/androidapp/MainActivity.kt
+++ android/app/src/main/kotlin/net/subroh0508/colormaster/androidapp/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.compose.ui.platform.setContent
 import androidx.databinding.DataBindingUtil
 import net.subroh0508.colormaster.androidapp.databinding.ActivityMainBinding
 import net.subroh0508.colormaster.androidapp.pages.Home
+import net.subroh0508.colormaster.model.HexColor
+import net.subroh0508.colormaster.model.IdolColor
+import net.subroh0508.colormaster.model.UiModel
+import net.subroh0508.colormaster.model.ui.idol.Filters
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
@@ -19,6 +23,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { Home() }
+        val uiModel = UiModel.Search(
+            listOf(
+                UiModel.Search.IdolColorItem(IdolColor("Mitsumine_Yuika", "三峰結華", HexColor("3B91C4"))),
+                UiModel.Search.IdolColorItem(IdolColor("Hachimiya_Meguru", "八宮めぐる", HexColor("FFE012"))),
+                UiModel.Search.IdolColorItem(IdolColor("Higuchi_Madoka", "樋口円香",  HexColor("BE1E3E"))),
+                UiModel.Search.IdolColorItem(IdolColor("Arisugawa_Natsuha", "有栖川夏葉", HexColor("90E677"))),
+            ),
+            Filters.Empty,
+            error = null,
+        )
+
+        setContent { Home(uiModel) }
     }
 }
--- android/app/src/main/kotlin/net/subroh0508/colormaster/androidapp/pages/Home.kt
+++ android/app/src/main/kotlin/net/subroh0508/colormaster/androidapp/pages/Home.kt
@@ -1,6 +1,5 @@
 package net.subroh0508.colormaster.androidapp.pages
 
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -14,17 +13,22 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import net.subroh0508.colormaster.androidapp.R
-import net.subroh0508.colormaster.androidapp.components.atoms.DrawerHeader
+import net.subroh0508.colormaster.androidapp.components.atoms.*
 import net.subroh0508.colormaster.androidapp.components.molecules.DrawerMenuList
+import net.subroh0508.colormaster.androidapp.components.organisms.ColorLists
 import net.subroh0508.colormaster.androidapp.components.organisms.HomeTopBar
 import net.subroh0508.colormaster.androidapp.components.organisms.SearchBox
 import net.subroh0508.colormaster.androidapp.components.templates.ModalDrawerBackdrop
+import net.subroh0508.colormaster.model.HexColor
+import net.subroh0508.colormaster.model.IdolColor
+import net.subroh0508.colormaster.model.UiModel
 import net.subroh0508.colormaster.model.ui.idol.Filters
+import net.subroh0508.colormaster.model.ui.idol.SearchState
 
 @Composable
 @ExperimentalMaterialApi
 @ExperimentalLayout
-fun Home() {
+fun Home(uiModel: UiModel.Search) {
     ModalDrawerBackdrop(
         appBar = { drawerState ->
             HomeTopBar(
@@ -34,9 +38,7 @@ fun Home() {
         },
         drawerContent = { HomeDrawerContent() },
         backLayerContent = { BackLayerContent() },
-        frontLayerContent = {
-            Text("FrontLayerContent")
-        },
+        frontLayerContent = { FrontLayerContent(uiModel) },
     )
 }
 
@@ -81,10 +83,42 @@ private fun BackLayerContent() {
     }
 }
 
+@Composable
+private fun FrontLayerContent(uiModel: UiModel.Search) {
+    Column {
+        val modifier = Modifier.padding(8.dp)
+
+        SearchStateLabel(uiModel, modifier = modifier.fillMaxWidth())
+        ColorLists(uiModel.items, modifier = modifier)
+    }
+}
+
+@Composable
+private fun SearchStateLabel(
+    uiModel: UiModel.Search,
+    modifier: Modifier,
+) = when (uiModel.searchState) {
+    SearchState.RANDOM -> InfoAlert(message = stringResource(R.string.search_state_label_random), modifier = modifier)
+    SearchState.WAITING -> WarningAlert(message = stringResource(R.string.search_state_label_waiting), modifier = modifier)
+    SearchState.SEARCHED -> SuccessAlert(message = stringResource(R.string.search_state_label_searched, uiModel.items.size), modifier = modifier)
+    SearchState.ERROR -> ErrorAlert(message = stringResource(R.string.search_state_label_error, uiModel.error?.message ?: ""), modifier = modifier)
+}
+
 @Preview
 @Composable
 @ExperimentalMaterialApi
 @ExperimentalLayout
 fun PreviewHome() {
-    Home()
+    val uiModel = UiModel.Search(
+        listOf(
+            UiModel.Search.IdolColorItem(IdolColor("Mitsumine_Yuika", "三峰結華", HexColor("3B91C4"))),
+            UiModel.Search.IdolColorItem(IdolColor("Hachimiya_Meguru", "八宮めぐる", HexColor("FFE012"))),
+            UiModel.Search.IdolColorItem(IdolColor("Higuchi_Madoka", "樋口円香",  HexColor("BE1E3E"))),
+            UiModel.Search.IdolColorItem(IdolColor("Arisugawa_Natsuha", "有栖川夏葉", HexColor("90E677"))),
+        ),
+        Filters.Empty,
+        error = null,
+    )
+
+    Home(uiModel)
 }
