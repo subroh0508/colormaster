38827d56bb9320e5d13be7d70f9fec8d1565fddd
subroh0508 <subroh.0508@gmail.com>
2020-10-12 03:12:49 +0900

    [delete]Remove UiModel.Search

--- shared/model/src/commonMain/kotlin/net/subroh0508/colormaster/model/UiModel.kt
+++ shared/model/src/commonMain/kotlin/net/subroh0508/colormaster/model/UiModel.kt
@@ -1,37 +1,6 @@
 package net.subroh0508.colormaster.model
 
-import net.subroh0508.colormaster.model.ui.idol.Filters
-import net.subroh0508.colormaster.model.ui.idol.SearchState
-
 abstract class UiModel {
-    data class Search(
-        val items: List<IdolColorItem>,
-        val filters: Filters,
-        val error: Throwable?,
-        val isLoading: Boolean = false
-    ) {
-        companion object {
-            val INITIALIZED = Search(emptyList(), Filters.Empty, null)
-        }
-
-        val searchState get() = when {
-            error != null -> SearchState.ERROR
-            isLoading -> SearchState.WAITING
-            filters == Filters.Empty -> SearchState.RANDOM
-            else -> SearchState.SEARCHED
-        }
-        val selected: List<IdolColor> get() = items.mapNotNull { it.takeIf(IdolColorItem::selected)?.idolColor }
-
-        data class IdolColorItem(
-            val idolColor: IdolColor,
-            val selected: Boolean
-        ) {
-            constructor(idolColor: IdolColor) : this(idolColor, false)
-
-            val itemId: Long get() = idolColor.id.hashCode().toLong()
-        }
-    }
-
     data class FullscreenPreview(
         val items: List<IdolColor>,
         val error: Throwable?,
--- shared/model/src/commonMain/kotlin/net/subroh0508/colormaster/model/ui/idol/SearchState.kt
+++
@@ -1,5 +0,0 @@
-package net.subroh0508.colormaster.model.ui.idol
-
-enum class SearchState {
-    RANDOM, SEARCHED, WAITING, ERROR
-}
