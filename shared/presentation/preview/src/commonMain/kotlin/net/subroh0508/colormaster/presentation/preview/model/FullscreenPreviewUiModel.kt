711b89bff56923ec720004852bd787d01843ae90
subroh0508 <subroh.0508@gmail.com>
2020-10-12 18:23:07 +0900

    [add]Add FullscreenPreviewUiModel

---
+++ shared/presentation/preview/src/commonMain/kotlin/net/subroh0508/colormaster/presentation/preview/model/FullscreenPreviewUiModel.kt
@@ -0,0 +1,20 @@
+package net.subroh0508.colormaster.presentation.preview.model
+
+import net.subroh0508.colormaster.model.IdolColor
+import net.subroh0508.colormaster.utilities.LoadState
+
+data class FullscreenPreviewUiModel(
+    val items: List<IdolColor>,
+    val error: Throwable? = null,
+    val isLoading: Boolean = false,
+) {
+    companion object {
+        val INITIALIZED = FullscreenPreviewUiModel(listOf())
+
+        operator fun invoke(loadState: LoadState) = FullscreenPreviewUiModel(
+            loadState.getValueOrNull<List<IdolColor>>() ?: listOf(),
+            loadState.getErrorOrNull(),
+            loadState.isLoading,
+        )
+    }
+}
