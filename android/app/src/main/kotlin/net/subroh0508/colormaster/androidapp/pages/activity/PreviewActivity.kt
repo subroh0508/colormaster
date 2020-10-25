fc8244950fafa1a0fdc764f64f7c3fe56e2888e6
subroh0508 <subroh.0508@gmail.com>
2020-10-25 20:17:31 +0900

    [edit]Add PenlightController to PreviewActivity

--- android/app/src/main/kotlin/net/subroh0508/colormaster/androidapp/pages/activity/PreviewActivity.kt
+++ android/app/src/main/kotlin/net/subroh0508/colormaster/androidapp/pages/activity/PreviewActivity.kt
@@ -1,10 +1,15 @@
 package net.subroh0508.colormaster.androidapp.pages.activity
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.ExperimentalLayout
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.platform.setContent
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import net.subroh0508.colormaster.androidapp.ScreenType
 import net.subroh0508.colormaster.androidapp.pages.Preview
 import net.subroh0508.colormaster.androidapp.previewIdolIds
 import net.subroh0508.colormaster.androidapp.screenType
@@ -20,11 +25,32 @@ class PreviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent { Preview(intent.screenType, viewModel) }
+
+        if (intent.screenType == ScreenType.Penlight) {
+            lifecycle.addObserver(penlightController)
+        }
     }
 
     override fun onResume() {
         super.onResume()
 
         viewModel.fetch(intent.previewIdolIds)
+    }
+
+    private val penlightController = object : LifecycleObserver {
+        @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        fun keepScreenOn() {
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        fun brightnessFull() {
+            window.attributes.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        fun brightnessNone() {
+            window.attributes.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE
+        }
     }
 }
