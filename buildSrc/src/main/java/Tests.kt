d4498c41d83f668ea844a9720bec97720bdf20c6
subroh0508 <subroh.0508@gmail.com>
2021-01-05 01:12:47 +0900

    [Add]Add jsNodeTestReport task

--- buildSrc/src/main/java/Tests.kt
+++ buildSrc/src/main/java/Tests.kt
@@ -2,6 +2,8 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
 import org.gradle.api.tasks.testing.TestReport
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
 
 fun Project.testDebugUnitTestReport() {
     tasks.create("testDebugUnitTestReport", TestReport::class) {
@@ -11,6 +13,19 @@ fun Project.testDebugUnitTestReport() {
             afterEvaluate {
                 val test = tasks.findByName("testDebugUnitTest") as? Test
                 if (test != null) reportOn(test)
+            }
+        }
+    }
+}
+
+fun Project.jsNodeTestReport() {
+    tasks.create("jsNodeTestReport", TestReport::class) {
+        destinationDir = file("$buildDir/reports/allTests/jsNodeTest")
+
+        gradle.afterProject {
+            afterEvaluate {
+                val test = tasks.findByName("jsNodeTest") as? KotlinJsTest
+                if (test != null) reportOn(test.binaryResultsDirectory)
             }
         }
     }
