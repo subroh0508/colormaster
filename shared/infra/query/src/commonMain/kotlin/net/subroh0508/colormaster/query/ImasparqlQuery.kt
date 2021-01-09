cb34f0ce668824aba0bedcb50499e45b4c449dca
subroh0508 <subroh.0508@gmail.com>
2021-01-10 01:48:46 +0900

    [add]Add SuggestLiveQuery

--- shared/infra/query/src/commonMain/kotlin/net/subroh0508/colormaster/query/ImasparqlQuery.kt
+++ shared/infra/query/src/commonMain/kotlin/net/subroh0508/colormaster/query/ImasparqlQuery.kt
@@ -10,6 +10,7 @@ abstract class ImasparqlQuery {
         private const val PREFIX_IMAS = "PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>"
         private const val PREFIX_RDF = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
         private const val PREFIX_RDFS = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+        private const val PREFIX_XSD = "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
     }
 
     protected abstract val rawQuery: String
@@ -19,6 +20,7 @@ abstract class ImasparqlQuery {
         append(PREFIX_IMAS)
         append(PREFIX_RDF)
         append(PREFIX_RDFS)
+        append(PREFIX_XSD)
 
         append(rawQuery)
     }
---
+++ shared/infra/query/src/commonMain/kotlin/net/subroh0508/colormaster/query/SuggestLiveQuery.kt
@@ -0,0 +1,19 @@
+package net.subroh0508.colormaster.query
+
+class SuggestLiveQuery(
+    dateRange: Pair<String, String>?, name: String?
+)  : ImasparqlQuery() {
+    override val rawQuery = """
+        SELECT ?liveName WHERE {
+          ?live rdf:type imas:Live;
+            schema:name ?liveName;
+            schema:startDate ?startDate;
+            schema:eventStatus ?eventStatus.
+        FILTER (?eventStatus != schema:EventCancelled)
+        ${name?.let { "FILTER { regex(?liveName, '.*$it.*', 'i') }" } ?: ""}
+        ${dateRange?.let { (start, end) -> "FILTER { xsd:date('$start') <= ?startDate && ?startDate <= xsd:date('$end') }" } ?: ""}
+        }
+        ORDER BY ?startDate
+        LIMIT 5
+    """.trimIndentAndBr()
+}
