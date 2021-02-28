4001a8fe71050d4f7aa941f593a905643cff043f
subroh0508 <subroh.0508@gmail.com>
2021-02-28 14:18:25 +0900

    [add]Add AuthenticationClient (androidMain)

---
+++ shared/api/authentication/src/androidMain/kotlin/net/subroh0508/colormaster/api/authentication/AndroidAuthenticationClient.kt
@@ -0,0 +1,50 @@
+package net.subroh0508.colormaster.api.authentication
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.UserInfo
+import kotlinx.coroutines.tasks.await
+import net.subroh0508.colormaster.api.authentication.model.FirebaseUser
+import com.google.firebase.auth.FirebaseUser as RawFirebaseUser
+import net.subroh0508.colormaster.api.authentication.model.Provider
+
+actual class AuthenticationClient(
+    private val auth: FirebaseAuth,
+) {
+    actual val currentUser get() = auth.currentUser?.toDataClass()
+
+    actual suspend fun signInAnonymously() = auth.signInAnonymously().await().user?.toDataClass() ?: throw IllegalStateException()
+
+    actual suspend fun signInWithGoogle(idToken: String): FirebaseUser {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+
+        return auth.signInWithCredential(credential).await().user?.toDataClass() ?: throw IllegalStateException()
+    }
+
+    actual suspend fun linkWithGoogle(idToken: String): FirebaseUser {
+        val rawUser = auth.currentUser ?: throw IllegalStateException()
+        if (rawUser.providerData.map(UserInfo::getProviderId).contains(Provider.PROVIDER_GOOGLE)) {
+            return rawUser.toDataClass()
+        }
+
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        return rawUser.linkWithCredential(credential).await().user?.toDataClass() ?: throw IllegalStateException()
+    }
+
+    actual suspend fun unlinkWithGoogle() =
+        auth.currentUser?.unlink(Provider.PROVIDER_GOOGLE)?.await()?.user?.toDataClass() ?: throw IllegalStateException()
+
+    private fun getProviderData(): List<Provider> {
+        val rawUser = auth.currentUser ?: return listOf()
+        if (rawUser.isAnonymous) {
+            return listOf(Provider(Provider.PROVIDER_ANONYMOUS, null, null))
+        }
+
+        return rawUser.providerData.map { Provider(it.providerId, it.email, it.displayName) }
+    }
+
+    private fun RawFirebaseUser.toDataClass() = FirebaseUser(
+        uid,
+        this@AuthenticationClient.getProviderData(),
+    )
+}
