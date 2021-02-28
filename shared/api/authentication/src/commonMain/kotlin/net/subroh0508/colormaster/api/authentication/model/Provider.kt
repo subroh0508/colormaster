6948ac86200ce6b55a968ea41ae8d85d6663eb64
subroh0508 <subroh.0508@gmail.com>
2021-02-28 14:13:02 +0900

    [add]Add model to :shared:api:authentication

---
+++ shared/api/authentication/src/commonMain/kotlin/net/subroh0508/colormaster/api/authentication/model/Provider.kt
@@ -0,0 +1,13 @@
+package net.subroh0508.colormaster.api.authentication.model
+
+data class Provider(
+    val id: String,
+    val email: String?,
+    val displayName: String?,
+) {
+    companion object {
+        const val PROVIDER_ANONYMOUS = "anonymous"
+        const val PROVIDER_GOOGLE = "google.com"
+        const val PROVIDER_TWITTER = "twitter.com"
+    }
+}
