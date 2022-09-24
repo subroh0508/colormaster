fabbc3ce37d3938cc372f5ad873e5f2d1a876746
subroh0508 <subroh.0508@gmail.com>
2022-09-24 17:00:54 +0900

    [edit]Add TermsPage content

--- web/app/src/jsMain/kotlin/page/about/TermsPage.kt
+++ web/app/src/jsMain/kotlin/page/about/TermsPage.kt
@@ -1,12 +1,29 @@
 package page.about
 
 import androidx.compose.runtime.Composable
-import material.components.TopAppBarMainContent
-import org.jetbrains.compose.web.dom.Text
+import components.atoms.card.OutlinedCard
+import components.templates.StaticPageFrame
+import utilities.LocalI18n
+import utilities.invoke
 
 @Composable
 fun TermsPage(
     topAppBarVariant: String,
-) = TopAppBarMainContent(topAppBarVariant) {
-    Text("Hello, World!")
+) = StaticPageFrame(topAppBarVariant) {
+    val t = LocalI18n() ?: return@StaticPageFrame
+
+    OutlinedCard(
+        header = t("about.terms.top.title"),
+        rawHtml = t("about.terms.top.description"),
+    )
+
+    OutlinedCard(
+        header = t("about.terms.disclaimer.title"),
+        rawHtml = t("about.terms.disclaimer.description"),
+    )
+
+    OutlinedCard(
+        header = t("about.terms.cookie.title"),
+        rawHtml = t("about.terms.cookie.description"),
+    )
 }
