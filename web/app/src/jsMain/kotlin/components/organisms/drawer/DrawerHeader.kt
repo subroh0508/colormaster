16bdde3e111dc4964e997b8b4d711a88a005aaa3
subroh0508 <subroh.0508@gmail.com>
2022-10-08 20:10:09 +0900

    [fix]Replace App Version to constant

--- web/app/src/jsMain/kotlin/components/organisms/drawer/DrawerHeader.kt
+++ web/app/src/jsMain/kotlin/components/organisms/drawer/DrawerHeader.kt
@@ -3,17 +3,15 @@ package components.organisms.drawer
 import androidx.compose.runtime.Composable
 import material.components.Divider
 import org.jetbrains.compose.web.dom.Text
-import utilities.LocalI18n
-import utilities.invoke
+import utilities.APP_NAME
+import utilities.APP_VERSION
 import material.components.DrawerHeader as MaterialDrawerHeader
 
 @Composable
 fun DrawerHeader() {
-    val t = LocalI18n() ?: return
-
     MaterialDrawerHeader(
-        title = { Text(t("title")) },
-        subtitle = { Text(t("version")) },
+        title = { Text(APP_NAME) },
+        subtitle = { Text(APP_VERSION) },
     )
     Divider()
 }
