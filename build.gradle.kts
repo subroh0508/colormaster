b9f3657e7f6d02a7177cee97fe2fddeb964f6717
subroh0508 <subroh.0508@gmail.com>
2024-07-04 22:22:47 +0900

    Delete unnecessary maven repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://s01.oss.sonatype.org/content/repositories/releases")
-        maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
-        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
