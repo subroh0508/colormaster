bd1a6d08c61e870c430e4708939fa07146a72334
subroh0508 <subroh.0508@gmail.com>
2024-09-10 00:37:56 +0900

    Delete Node/Yarn version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,5 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.kotlin.js) apply false
     alias(libs.plugins.kotest) apply false
-    id("node-convensions")
     id("ci-report-all-tests")
 }
