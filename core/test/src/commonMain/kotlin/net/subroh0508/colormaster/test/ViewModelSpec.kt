b9c3ca3de03db686f89f16b5798298e16c97666c
subroh0508 <subroh.0508@gmail.com>
2024-09-29 18:27:01 +0900

    Delete unnecessary method

--- core/test/src/commonMain/kotlin/net/subroh0508/colormaster/test/ViewModelSpec.kt
+++
@@ -1,5 +0,0 @@
-package net.subroh0508.colormaster.test
-
-import io.kotest.core.spec.style.FunSpec
-
-expect abstract class ViewModelSpec() : FunSpec
--- core/test/src/jsMain/kotlin/net/subroh0508/colormaster/test/JsViewModelSpec.kt
+++
@@ -1,5 +0,0 @@
-package net.subroh0508.colormaster.test
-
-import io.kotest.core.spec.style.FunSpec
-
-actual abstract class ViewModelSpec actual constructor() : FunSpec()
